10837237a10a96c64d1a0247040b3ff751affd1a
Ziv Zamechek <ziv921@yahoo.com>
2016-05-23 15:31:06 -0400

    debugged duplications and create form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,17 @@ class EventsController < ApplicationController
     redirect_to event_path(@event)
   end
 
+  def new
+  #instantiate an empty event object and render new view template
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
   private
   def event_params
     # explicitly which params could be changed via the form mass assignment
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Events::Application.routes.draw do 
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  # get "events" => "events#index"
+  # get "events/new" => "events#new"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update"
+
+  resources :events
 end
